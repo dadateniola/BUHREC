cdf5a379b73b891d689342fb1ea39cfbe410edf4
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2024-02-28 23:35:10 +0100

    sidebar complete

---
+++ assets/scripts/common.js
@@ -0,0 +1,8 @@
+const body = document.body;
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+const selectWith = (p, e) => p.querySelector(e);
+const selectAllWith = (p, e) => p.querySelectorAll(e);
+const create = (e) => document.createElement(e);
+const root = (e) => getComputedStyle(select(":root")).getPropertyValue(e);
+const getStyle = (e, style) => window.getComputedStyle(e)[style];
